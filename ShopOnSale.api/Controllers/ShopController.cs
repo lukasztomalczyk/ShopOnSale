fa4a65f49bcd3b0407f12757f6ac02787fd7f65f
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-04-23 17:51:44 +0200

    wokr in progress

--- ShopOnSale.api/Controllers/ShopController.cs
+++ ShopOnSale.api/Controllers/ShopController.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using ShopOnSale.services.Context.Config;
+using ShopOnSale.services.Models;
 using ShopOnSale.services.Repository;
 
 namespace ShopOnSale.api.Controllers
 {
+    [Route("Products/ShowProducts")]
     public class ShopController : Controller
     {
         private ShopServices _shopServices;
@@ -10,6 +14,12 @@ namespace ShopOnSale.api.Controllers
         public ShopController(ShopServices shopServices)
         {
             this._shopServices = shopServices;
+        }
+
+        [HttpGet]
+        public IEnumerable<ItemModel> GetAllProducts()
+        {
+            return _shopServices.ShowAllProducts();
         }
     }
 }
